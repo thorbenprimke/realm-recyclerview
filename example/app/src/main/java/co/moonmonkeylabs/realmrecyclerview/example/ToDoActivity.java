cbae57c58fa109b659c5d0871de0c188470da110
Thorben Primke <thorben@primke.com>
2015-11-11 20:53:34 -0800

    Adds todo input dialog

--- example/app/src/main/java/co/moonmonkeylabs/realmrecyclerview/example/ToDoActivity.java
+++ example/app/src/main/java/co/moonmonkeylabs/realmrecyclerview/example/ToDoActivity.java
@@ -1,11 +1,18 @@
 package co.moonmonkeylabs.realmrecyclerview.example;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import co.moonmonkeylabs.realmrecyclerview.RealmRecyclerView;
 
@@ -25,9 +32,56 @@ public class ToDoActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                buildAndShowInputDialog();
             }
         });
+    }
+
+    private void buildAndShowInputDialog() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(ToDoActivity.this);
+        builder.setTitle("Create A Task");
+
+        LayoutInflater li = LayoutInflater.from(this);
+        View dialogView = li.inflate(R.layout.to_do_dialog_view, null);
+        final EditText input = (EditText) dialogView.findViewById(R.id.input);
+
+        builder.setView(dialogView);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addToDoItem(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        final AlertDialog dialog = builder.show();
+        input.setOnEditorActionListener(
+                new EditText.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                                (event.getAction() == KeyEvent.ACTION_DOWN &&
+                                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                            dialog.dismiss();
+                            addToDoItem(input.getText().toString());
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+    }
+
+    private void addToDoItem(String toDoItemText) {
+        if (toDoItemText == null || toDoItemText.length() == 0) {
+            Toast
+                    .makeText(this, "Empty ToDos cannot get stuff done!", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
     }
 }
