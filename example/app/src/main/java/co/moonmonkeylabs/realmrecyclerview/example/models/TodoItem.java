3cdada8fc03fe894d349e1796a7a31738b2c644e
Thorben Primke <thorben@primke.com>
2016-05-30 17:38:34 -0700

    Updates Example For RealmModel - TodoItem Model was changed to RealmModel

--- example/app/src/main/java/co/moonmonkeylabs/realmrecyclerview/example/models/TodoItem.java
+++ example/app/src/main/java/co/moonmonkeylabs/realmrecyclerview/example/models/TodoItem.java
@@ -1,12 +1,15 @@
 package co.moonmonkeylabs.realmrecyclerview.example.models;
 
+import io.realm.RealmModel;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.RealmClass;
 
 /**
  * The model for a to-do item.
  */
-public class TodoItem extends RealmObject {
+@RealmClass
+public class TodoItem implements RealmModel {
 
     @PrimaryKey
     private long id;
